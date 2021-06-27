6d415419491b0597b6ac7728d760a54efbec84a4
Michael Barron <michael.barron08@gmail.com>
2021-06-27 16:21:01 -0500

    firstcommit

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+for(var i = 1; i<101;i++){
+    if(3% == 0 && 5% == 0){
+        console.log('FizzBuzz')
+    } else if( 3 % == 0 ){
+        console.log('Fiz')
+    } else if( 5 % == 0){
+        console.log('Buzz')
+    } else {
+        console.log(i)
+    }
+
+}
